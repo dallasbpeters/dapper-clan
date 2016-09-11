e1f28f771ac2f3e545638b8f1a9caeb1b0363bd9
Mark Bradshaw <mark.bradshaw@gmail.com>
2016-09-10 21:57:15 -0500

    Use standard functions to let safari get happy

--- contents/scripts/main.jsx
+++ contents/scripts/main.jsx
@@ -1,15 +1,21 @@
 const React = require('react');
 const Member = require('./member');
 
-const Main = (props) => (
-  <div>
-    <h1>Clan Members</h1>
+const Main = function(props) {
+  return (
+    <div>
+      <h1>Clan Members</h1>
 
-    <ul>
-      { props.members.map((member) => <Member key={member.membershipId} member={member} />) }
-    </ul>
+      <ul>
+        {
+          props.members.map(function(member) {
+            return <Member key={member.membershipId} member={member} />;
+          })
+        }
+      </ul>
 
-  </div>
-);
+    </div>
+  );
+}
 
 module.exports = Main;
--- contents/scripts/member.jsx
+++ contents/scripts/member.jsx
@@ -1,11 +1,13 @@
 const React = require('react');
 
-const Member = ({ member }) => (
-  <li>
-    { member.user.displayName + ' - ' + member.user.userTitleDisplay + ' ' }
-    { member.isOriginalFounder && 'FOUNDER!!' }
-    ({ member.isMember ? 'Member' : 'Pending' })
-  </li>
-);
+const Member = function({ member }) {
+  return (
+    <li>
+      { member.user.displayName + ' - ' + member.user.userTitleDisplay + ' ' }
+      { member.isOriginalFounder && 'FOUNDER!!' }
+      ({ member.isMember ? 'Member' : 'Pending' })
+    </li>
+  );
+}
 
 module.exports = Member;
