d6046f2f41f97c62adc6023647f1ac3db3d6ed88
Dallas Peters <dallaspeters@gmail.com>
2016-09-12 12:02:51 -0500

    removes h1 from clan component.

--- contents/scripts/main.jsx
+++ contents/scripts/main.jsx
@@ -3,8 +3,6 @@ import Member from './member';
 
 const Main = (props) => (
   <div>
-    <h1>Clan Members</h1>
-
     <ul>
       { props.members.map((member) => <Member key={member.membershipId} member={member} />) }
     </ul>
