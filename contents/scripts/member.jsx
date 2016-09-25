063cd5f19c06b2ef7c5eec68bda4018ed50f53d1
Dallas Peters <dallaspeters@gmail.com>
2016-09-25 17:07:24 -0400

    character stuff

--- contents/scripts/member.jsx
+++ contents/scripts/member.jsx
@@ -1,4 +1,5 @@
-const React = require('react');
+import React from 'react';
+import Character from './character';
 
 const Member = ({ member }) => {
   if (!member || !member.displayName) {
@@ -6,9 +7,26 @@ const Member = ({ member }) => {
   }
 
   return (
-    <li>
-      <img className="member_avatar" src={`http://bungie.net/${member.user.profilePicturePath}`} alt="member avatar"/>
-      <span className="member_name">{member.displayName}</span>
+    <li className="member">
+      <h1 className="member__name">{member.displayName}</h1>
+      <p className="member__about">{member.user.about}</p>
+        <p className="member__about">Grimoire: {
+          member.data
+            ? member.data.grimoireScore
+            : '0'
+      }</p>
+    <ul>
+      {
+        member.data && member.data.characters.map(
+          (character) => (
+            <Character
+              key={character.characterBase.characterId}
+              character={character}
+            />
+          )
+        )
+      }
+    </ul>
 
     </li>
   );
